c1ed87e3f115f980937d8cb64c2be2c153ad0d00
bradmeyn <bradjmeyn@gmail.com>
2022-08-06 15:55:28 +1000

    set up error handler and mongo db atlas connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const dotenv = require('dotenv').config;
+const colors = require('colors');
+require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 8000;
 
+// Connect to database
+
+connectDB();
+
 const app = express();
+
+//parse data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('hello');
@@ -10,5 +21,7 @@ app.get('/', (req, res) => {
 
 //routes
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
