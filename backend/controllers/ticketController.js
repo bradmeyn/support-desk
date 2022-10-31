31c384eaccc21f774c1907cf46d11c6b0dd834af
bradmeyn <brad@redcrowdigital.com.au>
2022-11-01 08:24:32 +1100

    add delete and update controllers for tickets

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -8,24 +8,120 @@ const User = require('../models/userModel');
 // @route   GET /api/tickets
 // @access  Private
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'getTickets' });
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(404);
+    throw new Error('User not found');
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+
+  res.status(200).json(tickets);
 });
 
 // @desc    Create new ticket
 // @route   POST /api/tickets
 // @access  Private
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'createTicket' });
+  const { product, description } = req.body;
+
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: 'new',
+  });
+
+  res.status(200).json(ticket);
 });
 
 // @desc    Get ticket for current user
 // @route   GET /api/tickets/:id
 // @access  Private
 const getTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'getTicket' });
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  //check user id on ticket matches current logged in user
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not authorized');
+  }
+
+  res.status(200).json(ticket);
+});
+
+// @desc    Delete ticket
+// @route   DELETE /api/tickets/:id
+// @access  Private
+const deleteTicket = asyncHandler(async (req, res) => {
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  //check user id on ticket matches current logged in user
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not authorized');
+  }
+
+  await ticket.remove();
+
+  res.status(200).json({ success: true });
+});
+
+// @desc    Delete ticket
+// @route   PUT /api/tickets/:id
+// @access  Private
+const updateTicket = asyncHandler(async (req, res) => {
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error('Ticket not found');
+  }
+
+  //check user id on ticket matches current logged in user
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('Not authorized');
+  }
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error('Product & description are required');
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  await ticket.remove();
+
+  res.status(200).json(updatedTicket);
 });
 
 module.exports = {
+  getTicket,
+  deleteTicket,
+  updateTicket,
   getTickets,
   createTicket,
 };
